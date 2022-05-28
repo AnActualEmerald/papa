65a26891498e3cfdb992cfb32069c057958268c6
AnActualEmerald <emerald_actual@proton.me>
2022-05-28 02:06:50 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,68 @@
+use std::{
+    fs::{self, File},
+    path::Path,
+};
+
+use clap::{Command, Parser, Subcommand};
+use directories::ProjectDirs;
+
+mod utils;
+
+#[derive(Parser)]
+#[clap(name = "Papa")]
+#[clap(author = "AnAcutalEmerald <emerald_actual@proton.me>")]
+#[clap(version = env!("CARGO_PKG_VERSION"))]
+#[clap(about = "Command line mod manager for Northstar")]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Install {
+        #[clap(name = "MOD")]
+        #[clap(help = "Mod name in Author.ModName@version format")]
+        mod_name: String,
+    },
+}
+
+const BASE_URL: &'static str = "https://northstar.thunderstore.io/package/download";
+
+#[tokio::main]
+async fn main() {
+    let cli = Cli::parse();
+
+    let dirs = ProjectDirs::from("me", "greenboi", "papa").unwrap();
+    ensure_dirs(&dirs);
+
+    match cli.command {
+        Commands::Install { mod_name } => {
+            let url = utils::parse_mod_name(&mod_name).unwrap();
+            let path = dirs.cache_dir().join(format!("{}.zip", mod_name));
+
+            if let Some(f) = check_cache(&path) {
+                println!("Using cached version of {}", mod_name);
+                return;
+            }
+            match utils::download_file(format!("{}{}", BASE_URL, url), path).await {
+                Ok(_) => {}
+                Err(e) => eprintln!("{}", e),
+            }
+        }
+    }
+}
+
+fn check_cache(path: &Path) -> Option<File> {
+    let opt = fs::OpenOptions::new().read(true).open(path);
+    if let Ok(f) = opt {
+        Some(f)
+    } else {
+        None
+    }
+}
+
+fn ensure_dirs(dirs: &ProjectDirs) {
+    fs::create_dir_all(dirs.cache_dir()).unwrap();
+    fs::create_dir_all(dirs.config_dir()).unwrap();
+}
