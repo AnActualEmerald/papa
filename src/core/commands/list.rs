823f739886545979dd35eaa67f0d85d9329666e3
Emerald <emerald_actual@proton.me>
2023-03-27 18:50:06 -0400

    make list command know about disabled mods

--- src/core/commands/list.rs
+++ src/core/commands/list.rs
@@ -15,7 +15,10 @@ pub fn list(global: bool, all: bool) -> Result<()> {
     let mods = find_mods(CONFIG.install_dir()).context("Error finding mods")?;
     debug!("Found {} mods", mods.len());
     trace!("{:?}", mods);
+    let enabled_mods = get_enabled_mods(CONFIG.install_dir().join("..")).ok();
+
     let mut grouped_mods: BTreeMap<ModName, BTreeSet<String>> = BTreeMap::new();
+    let mut disabled: BTreeMap<ModName, BTreeSet<String>> = BTreeMap::new();
     for m in mods {
         let m = if let Err(e) = m {
             println!("Error reading mod: {}", e);
@@ -26,13 +29,24 @@ pub fn list(global: bool, all: bool) -> Result<()> {
 
         let local_name = m.mod_json.name.clone();
         let mn = m.into();
-        if let Some(group) = grouped_mods.get_mut(&mn) {
-            debug!("Adding submod {} to group {}", local_name, mn);
-            group.insert(local_name);
-        } else {
-            debug!("Adding group {} for sdubmod {}", mn, local_name);
-            let group = BTreeSet::from([local_name]);
-            grouped_mods.insert(mn, group);
+
+        let process_mod = |mod_group: &mut BTreeMap<ModName, BTreeSet<String>>| {
+            if let Some(group) = mod_group.get_mut(&mn) {
+                debug!("Adding submod {} to group {}", local_name, mn);
+                group.insert(local_name.clone());
+            } else {
+                debug!("Adding group {} for sdubmod {}", mn, local_name);
+                let group = BTreeSet::from([local_name.clone()]);
+                mod_group.insert(mn, group);
+            }
+        };
+
+        if let Some(em) = enabled_mods.as_ref() {
+            if em.is_enabled(&local_name) {
+                process_mod(&mut grouped_mods);
+            } else {
+                process_mod(&mut disabled);
+            }
         }
     }
 
@@ -44,6 +58,16 @@ pub fn list(global: bool, all: bool) -> Result<()> {
             println!("-  {}:", group.bright_blue().bold());
             for n in names {
                 println!("    {}", n.bright_cyan().bold());
+            }
+        }
+    }
+
+    if !disabled.is_empty() {
+        println!("Disabled mods: ");
+        for (group, names) in disabled {
+            println!("-  {}:", group.bright_red().bold());
+            for n in names {
+                println!("    {}", n.magenta().bold());
             }
         }
     }
