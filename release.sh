6517c7912ee5bf2233a6f3b61ee9b98174eab9a7
Emerald <emerald_actual@proton.me>
2023-07-07 12:21:05 -0400

    fix release script

--- release.sh
+++ release.sh
@@ -15,7 +15,7 @@ msg="# managed by release.sh"
 sed -E -i "s/^version = .* $msg$/version = \"${1#v}\" $msg/" Cargo.toml
 
 # generate changelog
-git cliff > CHANGELOG.md
+git cliff --tag "$1" > CHANGELOG.md
 
 git add -A && git commit -m "chore(release): prep for $1"
 git show
