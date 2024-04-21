61307088e5e663352d6d6e942674d4aa7eab294c
Emerald <emerald@mecha.garden>
2024-04-20 21:32:53 -0400

    chore: make nix flake buildable

--- flake.nix
+++ flake.nix
@@ -3,20 +3,41 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
-  }: let
-    pkgs = import nixpkgs {system = "x86_64-linux";};
-  in {
-    formatter.x86_64-linux = pkgs.alejandra;
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      buildDeps = with pkgs; [pkg-config openssl rustc];
+      cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+      rustPackage = features:
+        pkgs.rustPlatform.buildRustPackage {
+          inherit (cargoToml.package) name version;
+          src = ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "libthermite-0.8.1" = "sha256-DPc6Nt8BN0Q+t+bf3p171BiIXuAAcVBbve2rR1l9QTg=";
+            };
+          };
+          buildFeatures = features;
+          nativeBuildInputs = buildDeps;
+        };
+    in rec {
+      packages.papa = rustPackage "";
 
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      nativeBuildInputs = [pkgs.pkg-config];
-      buildInputs = [pkgs.rustc pkgs.openssl];
-      packages = [pkgs.just pkgs.cargo pkgs.cargo-watch];
-    };
-  };
+      packages.default = packages.papa;
+
+      formatter = pkgs.alejandra;
+
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = buildDeps;
+        packages = [pkgs.just pkgs.cargo pkgs.cargo-watch pkgs.rust-analyzer];
+      };
+    });
 }
